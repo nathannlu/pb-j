4a2b4c5153c719690fdc660e746c7b498aa19cbf
nathannlu <nathanlu_@hotmail.com>
2020-06-30 16:59:43 -0700

    Fixed syntax error

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -5,7 +5,7 @@ import Section1 from './Section1';
 import Section2 from './Section2';
 import Footer from '../Footer';
 
-export default function() = {
+export default function() {
 	return (
 		<>
 			<Navbar />
