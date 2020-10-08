796067a397e36aee79c130d40f5963af60d45254
nathannlu <nathanlu_@hotmail.com>
2020-10-08 15:43:51 -0700

    teamtown casestudy

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -12,20 +12,20 @@ export default function() {
 			<Header />
 			<CaseStudiesHeader />
 			<CaseStudiesItem
-				caption="Creative Technology"
+				caption="Design & Development"
 				title="HostGenius"
 				description="A brand revamp that demystifies HostGenius' mission."
 				src="/hostgenius-cover.png"
 				url="/work/hostgenius"
 			/>
 			<CaseStudiesItem
-				caption="Creative Technology"
+				caption="Design & Development"
 				title="ThriveBar"
 				description="Rethinking the accessibility of beauty services."
 				src="/thrivebar-cover.png"
 			/>
 			<CaseStudiesItem
-				caption="Creative Technology"
+				caption="Design & Development"
 				title="Teamtown"
 				description="Dashboard for founders to build their company upon"
 				src="/teamtown-cover.png"
--- src/components/Teamtown/index.js
+++ src/components/Teamtown/index.js
@@ -22,63 +22,39 @@ const HostGenius = () => {
 			<section className="py-32 container mx-auto">
 				<div className="w-2/3 mx-auto">
 					<h3 className="pb-8">
-						Helping HostGenius Expand with a Strong Brand.
+						Creating a Trustable Brand.
 					</h3>
 					<p className="pb-8">
-						HostGenius is the #1 Vancouver short-term Airbnb rental management service that provides a seamless hosting experience.
+						Teamtown is a global creative services company providing thousands of businesses with unlimited, high-quality design and illustration work at a flat rate. Teamtown was created to deliver reliable, affordable, and scalable creative content to any business.
 					</p>
 					<p className="pb-8">
-						As HostGenius is looking to expand their services, there is a mis-match between the company’s current visual identity and the target audience.
-					</p>
-					<p className="pb-8">
-						We developed the visual identity, website, and media asset with the goal of building a commuity.
+						PB+J pushed Teamtown on the international scene crafting a trustworthy brand that stands for quality designs and world-class customer service.
 					</p>
 				</div>
 			</section>
 			<div>
 				<img className="w-full" src="/teamtown-asset-1.png" />	
-				<div className="flex flex-wrap">
-					<img className="w-1/3" src="/hostgenius-asset-2.png" />
-					<img className="w-1/3" src="/hostgenius-asset-3.png" />
-					<img className="w-1/3" src="/hostgenius-asset-4.png" />
-				</div>
 			</div>
 			<section className="py-32 container mx-auto">
 				<div className="w-2/3 mx-auto">
 					<h3 className="pb-8">
-						Aligning website with their brand.
+						Developing a website that inspires trust
 					</h3>
 					<p className="pb-8">
-						Property management has become very corporate, We helped HostGenius differentiate themselves by being more humane with a youthful but professional injection.
+						One of the key factors that will contribute to the success of Teamtown will be the way their company is presented.
+					</p>
+					<p className="pb-8">
+						As almost all their clients will discover them through online advertisements, Teamtown's website has to appear trustworthy. We spent a great deal of time coming up with colours for a visual identity that appeals to corporate, but still has a design vibe.
 					</p>
 				</div>
 			</section>
 			<div>
 				<div className="flex flex-wrap">
-					<img className="w-1/2" src="/hostgenius-asset-5.png" />
-					<img className="w-1/2" src="/hostgenius-asset-6.png" />
+					<img className="w-1/2" src="/teamtown-asset-2.png" />
+					<img className="w-1/2" src="/teamtown-asset-3.png" />
 				</div>
-				<img className="w-full" src="/hostgenius-asset-7.png" />	
+				<img className="w-full" src="/teamtown-asset-4.png" />	
 			</div>
-			<section className="py-32 container mx-auto">
-				<div className="w-2/3 mx-auto">
-					<h3 className="pb-8">
-						Conclusion
-					</h3>
-					<p className="pb-8">
-						HostGenius is the #1 Vancouver short-term Airbnb rental management service that provides a seamless hosting experience.
-					</p>
-					<p className="pb-8">
-						As HostGenius is looking to expand their services, there is a mis-match between the company’s current visual identity and the target audience.
-					</p>
-					<p className="pb-8">
-						We developed the visual identity, website, and media asset with the goal of building a commuity.
-					</p>
-					<a target="_blank" href="https://hostgenius.ca" className="py-4 px-8 mt-8 bg-black text-white" style={{fontSize: '1.8rem', fontWeight: 500}}>
-						Visit hostgenius.ca
-					</a>
-				</div>
-			</section>
 			<Footer />
 		</div>
 	)
