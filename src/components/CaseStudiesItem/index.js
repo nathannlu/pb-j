e0aaa46b26e507fa571e68eb8e7462050c3c83e0
nathannlu <nathanlu_@hotmail.com>
2020-10-09 06:26:16 -0700

    case study items mobile-res

--- src/components/CaseStudiesItem/index.js
+++ src/components/CaseStudiesItem/index.js
@@ -8,12 +8,12 @@ const CaseStudiesItem = props => {
 		<Link to={props.url} className="CaseStudiesItem overflow-hidden">
 			<div className="divider"></div>
 			<img className="image" style={{zIndex: -1}} src={props.src} />
-			<div className="container mx-auto flex flex-wrap">
-				<div className="w-1/2 main">
+			<div className="container mx-auto flex flex-wrap h-screen md:h-auto">
+				<div className="w-full md:w-1/2 main">
 					<h4 className="uppercase pb-4">{props.caption}</h4>
 					<h3>{props.title}</h3>
 				</div>
-				<div className="w-1/2 info">
+				<div className="w-full md:w-1/2 info">
 					<h3>
 						{props.description}	
 					</h3>
